2f28970e3caba0c15b0b75d0836b0c483404b606
SlimTim10 <slimtim10@gmail.com>
2021-05-09 13:01:31 +0000

    Move Reflex FormData patch into own file

--- frontend/src/Problem/Convert.hs
+++ frontend/src/Problem/Convert.hs
@@ -6,17 +6,14 @@ import qualified Control.Lens as Lens
 import qualified Data.Text as T
 
 import qualified JSDOM.Types
-import qualified JSDOM.FormData as FormData
 import qualified Language.Javascript.JSaddle as JS
 import qualified Reflex.Dom.Core as R
 
+-- Import unofficial patch file
+import qualified Xhr.FormData as R'
+
 import qualified Problem.Types as Types
 import Global
-
-data FormValue blob
-  = FormValue_Text Text
-  | FormValue_File blob (Maybe Text) -- maybe filename
-  | FormValue_List [FormValue blob]
 
 widget
   :: forall t m.
@@ -35,46 +32,27 @@ widget options prbName editorContent = R.el "div" $ do
   convert :: R.Event t () <- R.button "Convert"
 
   let allData :: R.Dynamic t (Types.Options, Text, Text) = (\ops nm ec -> (ops, nm, ec)) <$> options <*> prbName <*> editorContent
-  formData :: R.Event t [Map Text (FormValue JSDOM.Types.File)] <- R.performEvent $ R.ffor (R.tag (R.current allData) convert) $ \(ops, nm, ec) -> do
+  formData :: R.Event t [Map Text (R'.FormValue JSDOM.Types.File)] <- R.performEvent $ R.ffor (R.tag (R.current allData) convert) $ \(ops, nm, ec) -> do
     let
       r = Types.random ops
       o = Types.output ops
       fs = Types.files ops
-      formDataText :: Map Text (FormValue JSDOM.Types.File) = (
-        "prbText" =: FormValue_Text ec
-        <> "prbName" =: FormValue_Text nm
-        <> "random" =: FormValue_Text (formBool r)
-        <> "outFlag" =: FormValue_Text o
-        <> "submit1" =: FormValue_Text "putDatabase" -- temporary
-        <> "multiplefiles" =: FormValue_List (map formFile fs)
+      formDataText :: Map Text (R'.FormValue JSDOM.Types.File) = (
+        "prbText" =: R'.FormValue_Text ec
+        <> "prbName" =: R'.FormValue_Text nm
+        <> "random" =: R'.FormValue_Text (formBool r)
+        <> "outFlag" =: R'.FormValue_Text o
+        <> "submit1" =: R'.FormValue_Text "putDatabase" -- temporary
+        <> "multiplefiles" =: R'.FormValue_List (map formFile fs)
         )
     return [formDataText]
   
-  responses :: R.Event t [R.XhrResponse] <- postForms "https://icewire.ca/uploadprb" formData
+  responses :: R.Event t [R.XhrResponse] <- R'.postForms "https://icewire.ca/uploadprb" formData
   let results :: R.Event t [Maybe Text] = map (Lens.view R.xhrResponse_responseText) <$> responses
   R.el "div" $ do
     result :: R.Dynamic t Text <- R.holdDyn "" $ T.concat . map (maybe "" id) <$> results
     return $ R.decodeText <$> result
   where
-    formFile f = FormValue_File (Types.file f) (Just (Types.name f))
+    formFile f = R'.FormValue_File (Types.file f) (Just (Types.name f))
     formBool True = "true"
     formBool False = "false"
-
--- | Performs a POST request with the provided FormData payload
-postForms
-  :: ( JSDOM.Types.IsBlob blob, R.HasJSContext (R.Performable m), JS.MonadJSM (R.Performable m)
-     , R.PerformEvent t m, R.TriggerEvent t m
-     , Traversable f)
-  => Text -- ^ The target url
-  -> R.Event t (f (Map Text (FormValue blob))) -- ^ Maps of text keys and values that will be sent as "FormData"
-  -> m (R.Event t (f R.XhrResponse))
-postForms url payload = do
-  R.performMkRequestsAsync $ R.ffor payload $ \fs -> for fs $ \u -> JS.liftJSM $ do
-    fd <- FormData.newFormData Nothing
-    iforM_ u $ \k v -> appendFormValue k v fd
-    return $ R.xhrRequest "POST" url $ R.def & R.xhrRequestConfig_sendData .~ fd
-  where
-    appendFormValue k v fd = case v of
-      FormValue_Text t -> FormData.append fd k t
-      FormValue_File b fn -> FormData.appendBlob fd k b fn
-      FormValue_List vs' -> forM_ vs' $ \v' -> appendFormValue k v' fd
