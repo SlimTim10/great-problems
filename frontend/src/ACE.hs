2563464f2642a0808a524b01a6764707e03242d0
SlimTim10 <slimtim10@gmail.com>
2021-05-14 21:06:38 +0000

    Working on Ace
    Ace editor set up and working, but currently using hard-coded values for mode and theme

---
+++ frontend/src/ACE.hs
@@ -0,0 +1,363 @@
+-- Taken from
+-- https://github.com/reflex-frp/reflex-dom-ace
+-- with modifications
+
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE UndecidableInstances  #-}
+
+{-|
+
+Basic support for using the ACE editor with Reflex.
+
+IMPORTANT NOTE:
+
+This currently does not work if your app is using reflex-dom's
+mainWidgetWithHead or mainWidgetWithCss.
+
+Example usage:
+
+    ace <- divClass "yourACEWrapperDiv" $ -- wrapper div not required
+      aceWidget def (AceDynConfig Nothing) never "initial editor contents"
+
+    -- The rest is optional and lets you change what's in the editor on the fly
+    -- fly without redrawing the widget.
+    withAceInstance ace (setValueACE <$> updatesToContents)
+    holdDyn iv $ leftmost
+      [ updatesToContents
+      , updated (aceValue ace)
+      ]
+
+-}
+
+module ACE where
+
+------------------------------------------------------------------------------
+import           Control.Lens                       ((^.))
+import           Control.Monad                      (unless, void)
+import           Control.Monad.Trans
+import           Data.Default
+import           Data.Map                           (Map)
+import           Data.Maybe                         (fromMaybe)
+import           Data.Text                          (Text)
+import qualified Data.Text                          as T
+import           GHCJS.DOM.Types                    (JSVal)
+import           Language.Javascript.JSaddle        (asyncFunction,
+                                                     fromJSValUnchecked, js,
+                                                     js0, js1, js2, jsg)
+import           Language.Javascript.JSaddle.Object (MakeObject (..), create,
+                                                     (<#))
+import           Language.Javascript.JSaddle.Types  (MonadJSM, liftJSM)
+import           Language.Javascript.JSaddle.Value  (ToJSVal (..))
+import           Reflex
+import           Reflex.Dom.Core                    hiding (Element,
+                                                     fromJSString)
+import qualified Util
+------------------------------------------------------------------------------
+
+
+data AceTheme
+  = AceTheme_Chrome
+  | AceTheme_Clouds
+  | AceTheme_CrimsonEditor
+  | AceTheme_Dawn
+  | AceTheme_Dreamweaver
+  | AceTheme_Eclipse
+  | AceTheme_Github
+  | AceTheme_Iplastic
+  | AceTheme_SolarizedLight
+  | AceTheme_Textmate
+  | AceTheme_Tomorrow
+  | AceTheme_Xcode
+  | AceTheme_Kuroir
+  | AceTheme_Katzenmilch
+  | AceTheme_Sqlserver
+  | AceTheme_Ambiance
+  | AceTheme_Chaos
+  | AceTheme_CloudsMidnight
+  | AceTheme_Cobalt
+  | AceTheme_Gruvbox
+  | AceTheme_IdleFingers
+  | AceTheme_KrTheme
+  | AceTheme_Merbivore
+  | AceTheme_MerbivoreSoft
+  | AceTheme_MonoIndustrial
+  | AceTheme_Monokai
+  | AceTheme_PastelOnDark
+  | AceTheme_SolarizedDark
+  | AceTheme_Terminal
+  | AceTheme_TomorrowNight
+  | AceTheme_TomorrowNightBlue
+  | AceTheme_TomorrowNightBright
+  | AceTheme_TomorrowNightEighties
+  | AceTheme_Twilight
+  | AceTheme_VibrantInk
+  deriving (Eq,Ord,Enum,Bounded)
+
+instance Show AceTheme where
+    show AceTheme_Ambiance              = "ambiance"
+    show AceTheme_Chaos                 = "chaos"
+    show AceTheme_Chrome                = "chrome"
+    show AceTheme_Clouds                = "clouds"
+    show AceTheme_CloudsMidnight        = "clouds_midnight"
+    show AceTheme_Cobalt                = "cobalt"
+    show AceTheme_CrimsonEditor         = "crimson_editor"
+    show AceTheme_Dawn                  = "dawn"
+    show AceTheme_Dreamweaver           = "dreamweaver"
+    show AceTheme_Eclipse               = "eclipse"
+    show AceTheme_Github                = "github"
+    show AceTheme_Gruvbox               = "gruvbox"
+    show AceTheme_IdleFingers           = "idle_fingers"
+    show AceTheme_Iplastic              = "iplastic"
+    show AceTheme_Katzenmilch           = "katzenmilch"
+    show AceTheme_KrTheme               = "kr_theme"
+    show AceTheme_Kuroir                = "kuroir"
+    show AceTheme_Merbivore             = "merbivore"
+    show AceTheme_MerbivoreSoft         = "merbivore_soft"
+    show AceTheme_MonoIndustrial        = "mono_industrial"
+    show AceTheme_Monokai               = "monokai"
+    show AceTheme_PastelOnDark          = "pastel_on_dark"
+    show AceTheme_SolarizedDark         = "solarized_dark"
+    show AceTheme_SolarizedLight        = "solarized_light"
+    show AceTheme_Sqlserver             = "sqlserver"
+    show AceTheme_Terminal              = "terminal"
+    show AceTheme_Textmate              = "textmate"
+    show AceTheme_Tomorrow              = "tomorrow"
+    show AceTheme_TomorrowNight         = "tomorrow_night"
+    show AceTheme_TomorrowNightBlue     = "tomorrow_night_blue"
+    show AceTheme_TomorrowNightBright   = "tomorrow_night_bright"
+    show AceTheme_TomorrowNightEighties = "tomorrow_night_eighties"
+    show AceTheme_Twilight              = "twilight"
+    show AceTheme_VibrantInk            = "vibrant_ink"
+    show AceTheme_Xcode                 = "xcode"
+
+
+data AceConfig = AceConfig
+    { _aceConfigElemAttrs       :: Map Text Text
+    , _aceConfigBasePath        :: Maybe Text
+    , _aceConfigMode            :: Maybe Text
+    , _aceConfigWordWrap        :: Bool
+    , _aceConfigShowPrintMargin :: Bool
+    }
+
+
+data AceDynConfig = AceDynConfig
+    { _aceDynConfigTheme :: Maybe AceTheme
+    }
+
+
+instance Default AceConfig where
+    def = AceConfig def def def False False
+
+
+newtype AceInstance = AceInstance { unAceInstance :: JSVal }
+
+
+data ACE t = ACE
+    { aceRef   :: Dynamic t (Maybe AceInstance)
+    , aceValue :: Dynamic t Text
+    }
+
+
+------------------------------------------------------------------------------
+-- The type of editor session line annotation.
+data AnnotationType = AnnotationError
+                    | AnnotationWarning
+                    deriving (Show, Read)
+
+------------------------------------------------------------------------------
+instance ToJSVal AnnotationType where
+  toJSVal AnnotationError   = toJSVal ("error":: Text)
+  toJSVal AnnotationWarning = toJSVal ("warning" :: Text)
+
+
+------------------------------------------------------------------------------
+-- A line annotation for marking a specific line within the editor session as
+-- an error or a warning.
+data Annotation = Annotation { annotationRow    :: Int
+                             , annotationColumn :: Int
+                             , annotationText   :: Text
+                             , annotationType   :: AnnotationType
+                             } deriving (Show, Read)
+
+
+------------------------------------------------------------------------------
+instance MakeObject Annotation where
+  makeObject (Annotation row col txt typ) = do
+    o <- create
+    (o <# ("row" :: Text)   ) row
+    (o <# ("column" :: Text)) col
+    (o <# ("text" :: Text)  ) txt
+    (o <# ("type" :: Text)  ) typ
+    return o
+
+
+instance ToJSVal Annotation where
+  toJSVal = (toJSVal =<<) . makeObject
+
+
+------------------------------------------------------------------------------
+startACE :: MonadJSM m => Text -> AceConfig -> m AceInstance
+startACE containerId ac = liftJSM $ do
+  aceVal <- jsg ("ace" :: Text)
+  let [basePath, mode] = map (fromMaybe (T.pack "") . ($ ac))
+                              [_aceConfigBasePath, _aceConfigMode]
+  -- Set the base path if given
+  unless (T.null basePath) $ do
+    config <- aceVal ^. js ("config" :: Text)
+    void $ config ^. js2 ("set" :: Text) ("basePath" :: Text) basePath
+  -- Start and return an editing session
+  editor <- aceVal ^. js1 ("edit" :: Text) containerId
+  -- Set the mode if given
+  session <- editor ^. js ("session" :: Text)
+  void $ session ^. js1 ("setMode" :: Text) ("ace/mode/latex" :: Text)
+  void $ editor ^. js1 ("setTheme" :: Text) ("ace/theme/clouds" :: Text)
+  Util.consoleLog $ editor ^. js0 ("getTheme" :: Text)
+  Util.consoleLog $ session ^. js0 ("getMode" :: Text)
+  let aceInst  = AceInstance editor
+  setUseWrapMode (_aceConfigWordWrap ac) aceInst
+  setShowPrintMargin (_aceConfigShowPrintMargin ac) aceInst
+  return aceInst
+
+
+------------------------------------------------------------------------------
+moveCursorToPosition :: MonadJSM m => (Int, Int) -> AceInstance -> m ()
+moveCursorToPosition (r, c) (AceInstance ace) =
+  liftJSM $ void $ ace ^. js2 ("gotoLine" :: Text) r c
+
+
+------------------------------------------------------------------------------
+setThemeACE :: MonadJSM m => Maybe AceTheme -> AceInstance -> m ()
+setThemeACE Nothing      _                 = return ()
+setThemeACE (Just theme) (AceInstance ace) =
+  liftJSM $ void $ ace ^. js1 ("setTheme" :: Text) themeStr
+  where themeStr = "ace/theme/" <> show theme
+
+
+------------------------------------------------------------------------------
+setModeACE :: MonadJSM m => Text -> AceInstance -> m ()
+setModeACE mode (AceInstance ace) = liftJSM $ do
+  session <- ace ^. js ("session" :: Text)
+  void $ session ^. js1 ("setMode" :: Text) mode
+
+
+------------------------------------------------------------------------------
+setUseWrapMode :: MonadJSM m => Bool -> AceInstance -> m ()
+setUseWrapMode shouldWrap (AceInstance ace) = liftJSM $ do
+  session <- ace ^. js0 ("getSession" :: Text)
+  void $ session ^. js1 ("setUseWrapMode" :: Text) shouldWrap
+
+
+------------------------------------------------------------------------------
+setShowPrintMargin :: MonadJSM m => Bool -> AceInstance -> m ()
+setShowPrintMargin shouldShow (AceInstance ace) =
+  liftJSM $ void $ ace ^. js2 ("setOption" :: Text) ("showPrintMargin" :: Text) shouldShow
+
+
+------------------------------------------------------------------------------
+setUseWorker :: MonadJSM m => Bool -> AceInstance -> m ()
+setUseWorker shouldUse (AceInstance ace) =
+  liftJSM $ void $ ace ^. js2 ("setOption" :: Text) ("useWorker" :: Text) shouldUse
+
+
+------------------------------------------------------------------------------
+setAnnotations :: MonadJSM m => [Annotation] -> AceInstance -> m ()
+setAnnotations as (AceInstance ace) = liftJSM $ do
+  session <- ace ^. js0 ("getSession" :: Text)
+  annotations <- toJSValListOf as
+  void $ session ^. js1 ("setAnnotations" :: Text) annotations
+
+
+------------------------------------------------------------------------------
+setConfigACE :: MonadJSM m => Text -> Text -> AceInstance -> m ()
+setConfigACE t1 t2 (AceInstance ace) = liftJSM $ do
+  cfg <- ace ^. js ("config" :: Text)
+  void $ cfg ^. js2 ("set" :: Text) t1 t2
+
+
+------------------------------------------------------------------------------
+getValueACE :: MonadJSM m => AceInstance -> m Text
+getValueACE (AceInstance ace) =
+  liftJSM $ ace ^. js0 ("getValue" :: Text) >>= fromJSValUnchecked
+
+
+------------------------------------------------------------------------------
+setValueACE :: MonadJSM m => Text -> AceInstance -> m ()
+setValueACE t (AceInstance ace) =
+  liftJSM $ void $ ace ^. js2 ("setValue" :: Text) t (-1 :: Int)
+
+
+------------------------------------------------------------------------------
+setupValueListener
+  :: ( MonadJSM (Performable m)
+     , DomBuilder t m
+     , PostBuild t m
+     , TriggerEvent t m
+     , PerformEvent t m
+     )
+  => AceInstance
+  -> m (Event t Text)
+setupValueListener (AceInstance ace) = do
+  pb  <- getPostBuild
+  let act cb = liftJSM $ do
+        jscb <- asyncFunction $ \_ _ _ ->
+          getValueACE (AceInstance ace) >>= liftIO . cb
+        void $ ace ^. js2 ("on" :: Text) ("change" :: Text) jscb
+  performEventAsync (act <$ pb)
+
+
+------------------------------------------------------------------------------
+-- | Main entry point
+--
+-- IMPORTANT NOTE:
+--
+-- This currently does not work if your app is using reflex-dom's
+-- mainWidgetWithHead or mainWidgetWithCss.
+aceWidget
+    :: ( DomBuilder t m
+       , PostBuild t m
+       , MonadHold t m
+       , MonadJSM m
+       , TriggerEvent t m
+       , PerformEvent t m
+       , MonadJSM (Performable m)
+       )
+    => AceConfig -> AceDynConfig -> Event t AceDynConfig -> Text -> Text -> m (ACE t)
+aceWidget ac adc adcUps containerId initContents = do
+    aceInstance <- startACE containerId ac
+    onChange <- setupValueListener aceInstance
+    updatesDyn <- holdDyn initContents onChange
+
+    let ace = ACE (constDyn $ pure aceInstance) updatesDyn
+    setThemeACE (_aceDynConfigTheme adc) aceInstance
+    void $ withAceInstance ace (setThemeACE . _aceDynConfigTheme <$> adcUps)
+    return ace
+
+
+------------------------------------------------------------------------------
+-- | Convenient helper function for running functions that need an AceInstance.
+withAceInstance
+    :: PerformEvent t m
+    => ACE t
+    -> Event t (AceInstance -> Performable m ())
+    -> m (Event t ())
+withAceInstance ace evt = withAceInstance' ace (f <$> evt)
+  where
+    f _ Nothing  = return ()
+    f g (Just a) = g a
+
+
+------------------------------------------------------------------------------
+-- | More powerful function for running functions that need an AceInstance.
+withAceInstance'
+    :: PerformEvent t m
+    => ACE t
+    -> Event t (Maybe AceInstance -> Performable m a)
+    -> m (Event t a)
+withAceInstance' ace =
+  performEvent . attachPromptlyDynWith (flip ($)) (aceRef ace)
--- frontend/src/Problem/Editor.hs
+++ frontend/src/Problem/Editor.hs
@@ -1,18 +1,55 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Problem.Editor
   ( widget
   ) where
 
+import qualified ACE
+
+import qualified Language.Javascript.JSaddle.Types as JS
 import qualified Reflex.Dom.Core as R
 
 import Global
 
 widget
-  :: ( R.DomBuilder t m
+  :: forall t m.
+     ( R.DomBuilder t m
+     , R.TriggerEvent t m
+     , JS.MonadJSM (R.Performable m)
+     , JS.MonadJSM m
+     , R.PerformEvent t m
+     , R.PostBuild t m
+     , R.MonadHold t m
      )
   => R.Event t Text
   -> m (R.Dynamic t Text)
 widget forcedValue = do
-  t <- R.textAreaElement $ R.def
-    & R.setValue .~ forcedValue
-    & R.initialAttributes .~ ("class" =: "h-full w-full border")
-  return $ R.value t
+  let containerId = "editor"
+  void $ R.elAttr "div" (
+    "id" =: containerId
+    <> "class" =: "h-full w-full border"
+    ) R.blank
+  (script, _) <- R.elAttr' "script" (
+    "src" =: "/static/ace/ace.js"
+    <> "type" =: "text/javascript"
+    <> "charset" =: "utf-8"
+    ) R.blank
+  let scriptLoaded = () <$ R.domEvent R.Load script
+  let loading = R.el "p" $ R.text "Loading editor..." <&> const (R.constDyn "")
+  t' :: R.Dynamic t (R.Dynamic t Text) <- R.widgetHold loading $ R.ffor scriptLoaded $ const $ do
+    ace <- do
+      let
+        cfg = R.def
+          { ACE._aceConfigMode = Just "latex"
+          }
+      ACE.aceWidget cfg (ACE.AceDynConfig (Just ACE.AceTheme_Clouds)) R.never containerId ""
+    return $ ACE.aceValue ace
+  let t'' :: R.Event t Text = R.switchDyn $ R.updated <$> t'
+  t''' :: R.Dynamic t Text <- R.holdDyn "" t''
+  return $ t'''
+
+  ------------------------------------------------------------------------
+  -- t <- R.textAreaElement $ R.def
+  --   & R.setValue .~ forcedValue
+  --   & R.initialAttributes .~ ("class" =: "h-full w-full border")
+  -- return $ R.value t
