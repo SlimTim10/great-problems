025b7c8f2dbf229a5f11d0d887a94b330b8c5d28
SlimTim10 <slimtim10@gmail.com>
2021-05-29 18:19:01 +0000

    Add seeds

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -8,7 +8,13 @@ import qualified Database
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
   { _backend_run = \serve -> do
+      -- Connect to the database (for production)
+      -- conn <- Database.connect
+      
+      -- Database.setup clears and seeds the tables every time,
+      -- so it should only be used during development/testing
       conn <- Database.setup
+      
       serve $ const $ return ()
   , _backend_routeEncoder = fullRouteEncoder
   }
--- backend/src/Database/Seeds.hs
+++ backend/src/Database/Seeds.hs
@@ -5,6 +5,7 @@ module Database.Seeds where
 import qualified Database.PostgreSQL.Simple as SQL
 import qualified Database.PostgreSQL.Simple.SqlQQ as SqlQQ
 import qualified Crypto.PasswordStore
+import qualified Text.RawString.QQ as QQ
 import Global
 
 load :: SQL.Connection -> IO ()
@@ -37,7 +38,65 @@ load conn = do
       (?,?,?)
   |]
     [ (1, "Electrical Engineering", Nothing)
-    , (2, "Mathematics", Nothing)
-    , (3, "Calculus", Just 2)
+    , (2, "Circuit Design", Just 1)
+    , (3, "Mathematics", Nothing)
+    , (4, "Calculus", Just 3)
       :: (Integer, Text, Maybe Integer)
     ]
+
+  void $ SQL.executeMany conn [SqlQQ.sql|
+    INSERT INTO
+      problems(id, title, description, contents, thumnail_url, author_id, topic_id)
+    VALUES
+      (?,?,?,?,?,?,?)
+  |]
+    [
+      ( 1
+      , "Calculus Compound Rate"
+      , Nothing
+      , prbCalculusCompoundRate
+      , "https://cdn2.thecatapi.com/images/dbk.jpg"
+      , 1
+      , 4
+      )
+      :: ( Integer
+         , Text
+         , Maybe Text
+         , Text
+         , Text
+         , Integer
+         , Integer
+         )
+    ]
+
+prbCalculusCompoundRate :: Text
+prbCalculusCompoundRate = [QQ.r|
+\runParam{paramFormat = decimal}
+\runParam{paramSigDigits = 7}
+
+\runParam{R = 2;8;1}
+\runParam{P = 400;800;100}
+\runParam{Y = [4,5,6]}
+
+\question Find the present value of a continuous annuity at an annual rate of
+\val{R}\% compounded continuously for \val{Y} years if the payment at
+time $t$ is at the rate of \$\val{P} per year.
+\\
+\\
+\runSilent{Rdec = R/100}\\
+\runSilent{PV = (P/(-Rdec))*(exp(-Y*Rdec) - 1)}\\
+\begin{solutions}
+    \textbf{Solution}\\
+    \begin{align*}
+    P.V. &= \int_{0}^{T} f(t)e^{-rt}dt\\
+    &= \int_{0}^{\val{Y}} \val{P}e^{-\val{Rdec}t}dt\\
+    &= \frac{\val{P}}{-\val{Rdec}}e^{-\val{Rdec}t} \Bigr|_{0}^\val{Y}\\
+    &= \frac{\val{P}}{-\val{Rdec}}(e^{-\val{Y*Rdec}}-1)\\
+    &= \$\val{PV}
+    \end{align*}
+\end{solutions}
+\begin{answers}
+    \textbf{Answer}\\
+    P.V. = \$\val{PV}
+\end{answers}
+|]
