4385031b9e71bd7d5ccb51e758d78969215c5c6f
SlimTim10 <slimtim10@gmail.com>
2021-09-21 12:12:55 +0000

    First attempt at authentication
    Currently using JWT for sessions. Decision was made to change to database sessions and implement oauth later

--- backend/src/Auth.hs
+++ backend/src/Auth.hs
@@ -1,16 +1,74 @@
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DataKinds #-}
--- {-# LANGUAGE OverloadedStrings #-}
-module Auth where
+module Auth
+  ( authCheck
+  , signOut
+  , verifyJWTUser
+  , makeJWTCookie
+  , addCookie
+  , Auth.Auth
+  ) where
 
-import qualified Servant as Servant
-import Servant ((:>))
+import qualified Database.PostgreSQL.Simple as SQL
+import qualified Servant.Auth.Server as SAS
+import qualified Snap.Core as Snap
+import qualified Control.Monad.IO.Class as IO
+import qualified Data.ByteString as B
 
--- type Api = "/" :> ReqBody '[JSON] User :> Post '[JSON] User
+import qualified Common.Api.User as User
+import qualified Common.Api.Auth as Auth
+import qualified Database.Queries as Queries
+import Global
 
--- server :: Servant.Server Api
+authCheck
+  :: SQL.Connection
+  -> Auth.Auth
+  -> IO (SAS.AuthResult User.User)
+authCheck conn auth = maybe SAS.Indefinite SAS.Authenticated <$> Queries.authenticate conn auth
 
-type MyApi = "nothing" :> Servant.EmptyAPI
+signOut :: Snap.Snap ()
+signOut = do
+  removeCookie "user"
+  removeCookie "JWT-Cookie"
 
--- server :: Servant.Server MyApi _
-server = return ()
+verifyJWTUser :: SAS.JWTSettings -> Snap.Snap (Maybe User.User)
+verifyJWTUser jwtCfg = do
+  jwt :: Text <- Snap.readCookie "JWT-Cookie"
+  IO.liftIO $ SAS.verifyJWT jwtCfg (cs jwt)
+
+makeJWTCookie
+  :: ( IO.MonadIO m
+     , SAS.ToJWT v
+     )
+  => SAS.JWTSettings
+  -> v
+  -> m (Maybe B.ByteString)
+makeJWTCookie jwtCfg user = IO.liftIO $
+  SAS.makeSessionCookieBS SAS.defaultCookieSettings jwtCfg user
+
+mkCookie
+  :: Text -- ^ name
+  -> Text -- ^ value
+  -> Snap.Cookie
+mkCookie name value = Snap.Cookie
+  (cs name :: B.ByteString)
+  (cs value :: B.ByteString)
+  Nothing
+  Nothing
+  (Just "/")
+  False
+  False
+
+addCookie
+  :: Snap.MonadSnap m
+  => Text -- ^ cookie name
+  -> Text -- ^ cookie value
+  -> m ()
+addCookie name value =
+  Snap.modifyResponse
+  $ Snap.addResponseCookie
+  $ mkCookie name value
+
+removeCookie
+  :: Snap.MonadSnap m
+  => Text -- ^ cookie name
+  -> m ()
+removeCookie name = Snap.expireCookie $ mkCookie name ""
--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -10,9 +10,7 @@ import qualified Snap.Core as Snap
 import qualified Servant.Auth.Server as SAS
 import qualified Data.Aeson as JSON
 import qualified Data.Map as Map
-import qualified Database.PostgreSQL.Simple as SQL
 import qualified Data.Word as Word
-import qualified Data.ByteString as B
 
 import qualified Common.Route as Route
 import qualified Common.Api.Error as Error
@@ -20,15 +18,9 @@ import qualified Database
 import qualified Database.Queries as Queries
 import qualified Common.Api.User as User
 import qualified Common.Api.Role as Role
-import qualified Common.Api.Auth as Auth
 import qualified Common.Api.NewProblem as NewProblem
+import qualified Auth
 import Global
-
-authCheck
-  :: SQL.Connection
-  -> Auth.Auth
-  -> IO (SAS.AuthResult User.User)
-authCheck conn auth = maybe SAS.Indefinite SAS.Authenticated <$> Queries.authenticate conn auth
 
 maxRequestBodySize :: Word.Word64
 maxRequestBodySize = 2048
@@ -44,76 +36,65 @@ backend = Ob.Backend
       
       serve $ \case
         Route.BackendRoute_Missing :/ () -> return ()
-        Route.BackendRoute_Api :/ apiRoute -> case apiRoute of
-          Route.Api_Problems :/ subRoute -> case subRoute of
-            (Nothing, query) -> do
-              Snap.rqMethod <$> Snap.getRequest >>= \case
-                Snap.GET -> writeJSON =<< IO.liftIO (Queries.getProblems conn Nothing query)
-                Snap.POST -> do
-                  verifyJWTUser jwtCfg >>= \case
-                    Nothing -> writeJSON $ Error.mk "No access"
-                    Just user -> do
-                      rawBody <- Snap.readRequestBody maxRequestBodySize
-                      case JSON.decode rawBody :: Maybe NewProblem.NewProblem of
-                        Nothing -> writeJSON $ Error.mk "Invalid problem"
-                        Just newProblem -> do
-                          if
-                            NewProblem.author_id newProblem /= User.id user
-                            || not (User.role user == Role.Contributor || User.role user == Role.Moderator)
-                            then writeJSON $ Error.mk "No access"
-                            else writeJSON =<< IO.liftIO (Queries.addProblem conn newProblem)
-                _ -> return () -- TODO: implement put, delete
-            (Just problemId, query) -> writeJSON =<< IO.liftIO (Queries.getProblemById conn problemId query)
-          Route.Api_Topics :/ query -> do
-            topics <- case fromMaybe Nothing (Map.lookup "parent" query) of
-              Just "null" -> IO.liftIO (Queries.getRootTopics conn)
-              Just x -> case readMaybe (cs x) :: Maybe Integer of
-                Just parentId -> IO.liftIO (Queries.getTopicsByParentId conn parentId)
+        Route.BackendRoute_Api :/ apiRoute -> do
+          -- IO.liftIO $ print ("--------------------------------- user:" :: Text)
+          -- user :: Maybe User.User <- verifyJWTUser jwtCfg
+          -- IO.liftIO $ print ("--------------------------------- user:" :: Text)
+          -- IO.liftIO $ print user
+          -- when (isNothing user) signOut
+          case apiRoute of
+            Route.Api_Problems :/ subRoute -> case subRoute of
+              (Nothing, query) -> do
+                Snap.rqMethod <$> Snap.getRequest >>= \case
+                  Snap.GET -> writeJSON =<< IO.liftIO (Queries.getProblems conn Nothing query)
+                  Snap.POST -> do
+                    Auth.verifyJWTUser jwtCfg >>= \case
+                      Nothing -> writeJSON $ Error.mk "No access"
+                      Just user -> do
+                        rawBody <- Snap.readRequestBody maxRequestBodySize
+                        case JSON.decode rawBody :: Maybe NewProblem.NewProblem of
+                          Nothing -> writeJSON $ Error.mk "Invalid problem"
+                          Just newProblem -> do
+                            if
+                              NewProblem.author_id newProblem /= User.id user
+                              || not (User.role user == Role.Contributor || User.role user == Role.Moderator)
+                              then writeJSON $ Error.mk "No access"
+                              else writeJSON =<< IO.liftIO (Queries.addProblem conn newProblem)
+                  _ -> return () -- TODO: implement put, delete
+              (Just problemId, query) -> writeJSON =<< IO.liftIO (Queries.getProblemById conn problemId query)
+            Route.Api_Topics :/ query -> do
+              topics <- case fromMaybe Nothing (Map.lookup "parent" query) of
+                Just "null" -> IO.liftIO (Queries.getRootTopics conn)
+                Just x -> case readMaybe (cs x) :: Maybe Integer of
+                  Just parentId -> IO.liftIO (Queries.getTopicsByParentId conn parentId)
+                  Nothing -> IO.liftIO (Queries.getTopics conn)
                 Nothing -> IO.liftIO (Queries.getTopics conn)
-              Nothing -> IO.liftIO (Queries.getTopics conn)
-            writeJSON topics
-          Route.Api_Users :/ subRoute -> case subRoute of
-            Nothing -> writeJSON =<< IO.liftIO (Queries.getUsers conn)
-            Just userId -> writeJSON =<< IO.liftIO (Queries.getUserById conn userId)
-          Route.Api_TopicHierarchy :/ subRoute -> case subRoute of
-            Nothing -> writeJSON $ Error.mk "Not yet implemented"
-            Just topicId -> do
-              IO.liftIO (Queries.getTopicById conn topicId) >>= \case
-                Nothing -> writeJSON $ Error.mk "Topic not found"
-                Just topic -> writeJSON =<< IO.liftIO (Queries.getTopicHierarchy conn topic)
-          Route.Api_SignIn :/ () -> do
-            rawBody <- Snap.readRequestBody maxRequestBodySize
-            case JSON.decode rawBody :: Maybe Auth.Auth of
-              Just auth -> do
-                IO.liftIO (authCheck conn auth) >>= \case
-                  SAS.Authenticated user -> do
-                    makeJWTCookie jwtCfg user >>= \case
-                      Just rawJWTCookie -> do
-                        addCookie "user" (cs $ JSON.encode user)
-                        Snap.modifyResponse $ Snap.setHeader "Set-Cookie" rawJWTCookie
-                      Nothing -> writeJSON $ Error.mk "No cookie"
-                  _ -> writeJSON $ Error.mk "Incorrect email or password. Please try again."
-              Nothing -> writeJSON $ Error.mk "No auth in body"
-          Route.Api_SignOut :/ () -> do
-            removeCookie "user"
-            removeCookie "JWT-Cookie"
+              writeJSON topics
+            Route.Api_Users :/ subRoute -> case subRoute of
+              Nothing -> writeJSON =<< IO.liftIO (Queries.getUsers conn)
+              Just userId -> writeJSON =<< IO.liftIO (Queries.getUserById conn userId)
+            Route.Api_TopicHierarchy :/ subRoute -> case subRoute of
+              Nothing -> writeJSON $ Error.mk "Not yet implemented"
+              Just topicId -> do
+                IO.liftIO (Queries.getTopicById conn topicId) >>= \case
+                  Nothing -> writeJSON $ Error.mk "Topic not found"
+                  Just topic -> writeJSON =<< IO.liftIO (Queries.getTopicHierarchy conn topic)
+            Route.Api_SignIn :/ () -> do
+              rawBody <- Snap.readRequestBody maxRequestBodySize
+              case JSON.decode rawBody :: Maybe Auth.Auth of
+                Just auth -> do
+                  IO.liftIO (Auth.authCheck conn auth) >>= \case
+                    SAS.Authenticated user -> do
+                      Auth.makeJWTCookie jwtCfg user >>= \case
+                        Just rawJWTCookie -> do
+                          Auth.addCookie "user" (cs $ JSON.encode user)
+                          Snap.modifyResponse $ Snap.setHeader "Set-Cookie" rawJWTCookie
+                        Nothing -> writeJSON $ Error.mk "No cookie"
+                    _ -> writeJSON $ Error.mk "Incorrect email or password. Please try again."
+                Nothing -> writeJSON $ Error.mk "No auth in body"
+            Route.Api_SignOut :/ () -> Auth.signOut
   , Ob._backend_routeEncoder = Route.fullRouteEncoder
   }
-
-verifyJWTUser :: SAS.JWTSettings -> Snap.Snap (Maybe User.User)
-verifyJWTUser jwtCfg = do
-  jwt :: Text <- Snap.readCookie "JWT-Cookie"
-  IO.liftIO $ SAS.verifyJWT jwtCfg (cs jwt)
-
-makeJWTCookie
-  :: ( IO.MonadIO m
-     , SAS.ToJWT v
-     )
-  => SAS.JWTSettings
-  -> v
-  -> m (Maybe B.ByteString)
-makeJWTCookie jwtCfg user = IO.liftIO $
-  SAS.makeSessionCookieBS SAS.defaultCookieSettings jwtCfg user
 
 -- | Set MIME to 'application/json' and write given object into
 -- 'Response' body.
@@ -126,32 +107,3 @@ writeJSON a = do
 jsonResponse :: Snap.MonadSnap m => m ()
 jsonResponse = Snap.modifyResponse $
   Snap.setHeader "Content-Type" "application/json"
-
-mkCookie
-  :: Text -- ^ name
-  -> Text -- ^ value
-  -> Snap.Cookie
-mkCookie name value = Snap.Cookie
-  (cs name :: B.ByteString)
-  (cs value :: B.ByteString)
-  Nothing
-  Nothing
-  (Just "/")
-  False
-  False
-
-addCookie
-  :: Snap.MonadSnap m
-  => Text -- ^ cookie name
-  -> Text -- ^ cookie value
-  -> m ()
-addCookie name value =
-  Snap.modifyResponse
-  $ Snap.addResponseCookie
-  $ mkCookie name value
-
-removeCookie
-  :: Snap.MonadSnap m
-  => Text -- ^ cookie name
-  -> m ()
-removeCookie name = Snap.expireCookie $ mkCookie name ""
