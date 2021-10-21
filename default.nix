612a4a13143c787fe2c9f70538035f2f669837c5
SlimTim10 <slimtim10@gmail.com>
2021-10-21 14:48:45 -0400

    Use deps

--- default.nix
+++ default.nix
@@ -17,21 +17,8 @@
 with obelisk;
 project ./. ({ pkgs, hackGet, ... }: {
   overrides = with pkgs.haskell.lib; (self: super: {
-#    dotenv = dontCheck (self.callCabal2nix "dotenv"
-#      (pkgs.fetchFromGitHub {
-#        owner = "stackbuilders";
-#        repo = "dotenv-hs";
-#        rev = "b4cc2ac18d71a1e500f1ea8c180f723022b50651";
-#        sha256 = "0vgj5wn4p9wrs56aah49b57p6v95xz5j57mabadnrarg5ybq6wcc";
-#      }){});
-    wreq = dontCheck (self.callCabal2nix "wreq"
-      (pkgs.fetchFromGitHub {
-        owner = "haskell";
-        repo = "wreq";
-        rev = "2da0070625a680d5af59e36c3ca253ef6a80aea9";
-        sha256 = "1zdxpcs9hrjl5s9czd7y4hkqm2mn8gwcaha61r0crcgs8qj3sqfs";
-      }){});
     dotenv = dontCheck (self.callCabal2nix "dotenv" (hackGet ./dep/dotenv-hs) {});
+    wreq = dontCheck (self.callCabal2nix "wreq" (hackGet ./dep/wreq) {});
   });
   packages = {
     reflex-dom-ace = hackGet ./dep/reflex-dom-ace;
