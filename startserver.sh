bb03e6c4213c33c69803fd17b6f47a6f6613442c
SlimTim10 <slimtim10@gmail.com>
2021-10-23 08:28:08 -0400

    Log access and errors to disk

--- startserver.sh
+++ startserver.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 ADDRESS=$(hostname -I | awk '{print $1}')
 (
-  cd dist &&
-  sudo ./backend --hostname=greatproblems.ca --address=$ADDRESS --port=80 --ssl-address=$ADDRESS --ssl-port=443 --ssl-cert=/etc/letsencrypt/live/greatproblems.ca/fullchain.pem --ssl-chain-cert --verbose --ssl-key=/etc/letsencrypt/live/greatproblems.ca/privkey.pem
+	cd dist &&
+		sudo ./backend \
+			 --hostname=greatproblems.ca \
+			 --address=$ADDRESS \
+			 --port=80 \
+			 --ssl-address=$ADDRESS \
+			 --ssl-port=443 \
+			 --ssl-cert=/etc/letsencrypt/live/greatproblems.ca/fullchain.pem \
+			 --ssl-chain-cert \
+			 --ssl-key=/etc/letsencrypt/live/greatproblems.ca/privkey.pem \
+			 --access-log=/var/log/great-problems/access.log \
+			 --error-log=/var/log/great-problems/error.log \
 )
