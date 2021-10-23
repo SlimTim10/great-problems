e026a2d3ce8ec8134c4fce68b56c5603a9004825
SlimTim10 <slimtim10@gmail.com>
2021-10-23 08:55:19 -0400

    Log output

--- startserver.sh
+++ startserver.sh
@@ -12,5 +12,6 @@ ADDRESS=$(hostname -I | awk '{print $1}')
 			 --ssl-chain-cert \
 			 --ssl-key=/etc/letsencrypt/live/greatproblems.ca/privkey.pem \
 			 --access-log=/var/log/great-problems/access.log \
-			 --error-log=/var/log/great-problems/error.log \
+			 --error-log=/var/log/great-problems/error.log
+			 >> /var/log/great-problems/output.log
 )
