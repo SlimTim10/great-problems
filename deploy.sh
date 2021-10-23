c17c738aa644d1292a120ca36ce72fe43b285f39
SlimTim10 <slimtim10@gmail.com>
2021-10-22 23:03:39 -0400

    Add friendly prints to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo "Stopping server..."
 . stopserver.sh
+
+echo "Building app..."
 nix-build -A exe --no-out-link
 rm -rf dist
 mkdir dist
 ln -s $(nix-build -A exe --no-out-link)/* dist/
 cp -r config dist
+echo "App built in dist/"
+
+echo "Starting server..."
 . startserver.sh
