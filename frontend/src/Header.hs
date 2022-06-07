22ba38341265052ad718c31967893717c8a35c36
SlimTim10 <slimtim10@gmail.com>
2022-06-07 14:34:27 -0400

    Activate top nav search bar

--- frontend/src/Header.hs
+++ frontend/src/Header.hs
@@ -3,20 +3,20 @@ module Header
   ( widget
   ) where
 
-import Common.Lib.Prelude
-import qualified Frontend.Lib.Util as Util
-
+import qualified Web.KeyCode as Key
 import qualified Obelisk.Route.Frontend as Ob
 import qualified Reflex.Dom.Core as R
-import qualified MyReflex.Dom.Widget.Basic as R'
 import qualified "ghcjs-dom" GHCJS.DOM.Document as DOM
 import qualified Language.Javascript.JSaddle as JS
 import qualified Data.CaseInsensitive as CI
 
+import Common.Lib.Prelude
+import qualified Frontend.Lib.Util as Util
 import qualified Common.Route as Route
 import qualified Widget.Button as Button
 import qualified Common.Api.User as User
 import qualified Common.Api.Role as Role
+import qualified Common.Api.Search as Search
 
 widget
   :: forall t m js.
@@ -27,6 +27,7 @@ widget
      , R.HasDocument m
      , DOM.IsDocument (R.RawDocument (R.DomBuilderSpace m))
      , JS.MonadJSM m
+     , R.PostBuild t m
      )
   => m ()
 widget = R.elClass "header" "h-14 py-2 px-3 flex items-center justify-between border-b border-brand-light-gray" $ do
@@ -35,11 +36,24 @@ widget = R.elClass "header" "h-14 py-2 px-3 flex items-center justify-between bo
       $ R.text "Great Problems"
   Ob.routeLink (Route.FrontendRoute_Search :/ mempty) $ do
     Button.primary "Explore"
-  R'.elAttrClass
-    "input"
-    ("type" =: "search" <> "placeholder" =: "Search...")
-    "border rounded h-8 w-1/2 px-1"
-    $ R.blank
+
+  -- Search bar
+  searchTermInput <- R.inputElement
+    $ R.def & R.inputElementConfig_elementConfig . R.elementConfig_initialAttributes .~
+    ( "type" =: "search"
+      <> "placeholder" =: "Search..."
+      <> "class" =: "border rounded h-8 w-1/2 px-1"
+    )
+  let searchTerm :: R.Dynamic t (Maybe Text) = fmap textToMaybe . R.value $ searchTermInput
+  let params :: R.Dynamic t Search.Params = Search.Params
+        <$> searchTerm
+        <*> R.constDyn Nothing
+        <*> R.constDyn Nothing
+        <*> R.constDyn Nothing
+  Util.dynFor params $ \params' ->
+    Ob.setRoute $ (Route.FrontendRoute_Search :/ Search.paramsToQuery params')
+    <$ R.keydown Key.Enter searchTermInput
+    
   Util.getCurrentUser >>= \case
     Just user -> do
       when (User.role user == Role.Basic) $ do
