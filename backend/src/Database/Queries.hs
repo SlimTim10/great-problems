8a9f1dea53ff3dd19f23d95cc8aae6d6dcc087a1
SlimTim10 <slimtim10@gmail.com>
2022-06-07 13:13:16 -0400

    Order results by last updated

--- backend/src/Database/Queries.hs
+++ backend/src/Database/Queries.hs
@@ -104,11 +104,12 @@ getProblems conn routeQuery = do
     Just q -> return $ Just ("summary LIKE CONCAT('%', ?, '%')", SQL.toField q)
   let exprs = [topicExpr, authorExpr, statusExpr, queryExpr]
   let whereClause = mconcat . intersperse " AND " . map fst . catMaybes $ exprs
+  let orderByClause = " ORDER BY updated_at DESC "
   let whereParams = map snd . catMaybes $ exprs
   dbProblems :: [DbProblem.Problem] <-
     if not . all isNothing $ exprs
-    then SQL.query conn ("SELECT * FROM problems WHERE " <> whereClause) whereParams
-    else SQL.query_ conn "SELECT * FROM problems"
+    then SQL.query conn ("SELECT * FROM problems WHERE " <> whereClause <> orderByClause) whereParams
+    else SQL.query_ conn ("SELECT * FROM problems " <> orderByClause)
   problemAuthors :: [User.User] <- sequence (map (fetchProblemAuthor conn) dbProblems)
   problemTopics :: [Topic.Topic] <- sequence (map (fetchProblemTopic conn) dbProblems)
   problemTopicPaths :: [[Topic.Topic]] <- sequence (map (fetchProblemTopicPath conn) dbProblems)
