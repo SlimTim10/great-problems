b98641ad15cfc19ec099ed7744b8c20ed476f7db
SlimTim10 <slimtim10@gmail.com>
2022-09-13 19:02:30 -0400

    Update to latest problem2tex

--- dep-ext/problem2tex/default.nix
+++ dep-ext/problem2tex/default.nix
@@ -1,13 +1,15 @@
 { pkgs, buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "problem2tex";
-  version = "0.9.13";
+  version = "0.9.14";
 
   src = fetchFromGitHub {
     owner = "icewire314";
     repo = "problem2tex";
-    rev = "v${version}";
-    sha256 = "sha256-wjkwU91fqdS5qQeGXnfZuXwAP8ndqcg9fywCVSwQ4FQ=";
+    # rev = "v${version}";
+    rev = "a03359d5f297e224a29704b88df782a6b409ed5f";
+    # sha256 = pkgs.stdenv.lib.fakeSha256;
+    sha256 = "sha256-9Ne545st4NLofe+PFs8TRm2c9scAM9PhdGBqHg2p7EQ=";
   };
 
   # modSha256 = pkgs.stdenv.lib.fakeSha256;
