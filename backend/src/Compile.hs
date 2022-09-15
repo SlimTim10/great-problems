7858a482248059b2056a67ee1a814bb48439c841
SlimTim10 <slimtim10@gmail.com>
2022-09-15 14:53:56 -0400

    Minor change

--- backend/src/Compile.hs
+++ backend/src/Compile.hs
@@ -13,17 +13,17 @@ import qualified Common.Api.Figure as Figure
 import qualified Common.Api.Compile as Compile
 import qualified Common.Api.Error as Error
 
--- TODO: error handling (IO, problem2tex, emacs)
+-- This should be updated to handle IO errors.
+-- Ideally, any IO error should have its message printed in the console and an ambiguous text returned (e.g., Left "Something went wrong").
 compile
   :: Text -- ^ Problem contents
   -> Compile.RandomSeed -- Seed for randomized variables
   -> [Figure.BareFigure] -- Figures
   -> IO (Either Error.Error Text)
 compile contents randomSeed figures = do
-  tmpDir <- System.Directory.getTemporaryDirectory
   -- Do all of the work within a temporary directory (deleted after)
   -- e.g., /tmp/prb-e4bd89e5d00acdee
-  System.IO.Temp.withTempDirectory tmpDir "prb" $ \prbDir ->
+  System.IO.Temp.withSystemTempDirectory "prb" $ \prbDir ->
     System.Directory.withCurrentDirectory prbDir $ do
     -- Create files for problem2tex
     -- Problem: main.prb
