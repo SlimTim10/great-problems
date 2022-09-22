d91593ed0afb1a1c2f5921f48250656a9f3c4853
SlimTim10 <slimtim10@gmail.com>
2022-09-22 05:48:13 -0400

    Update shell script for build

--- startserver.sh
+++ startserver.sh
@@ -2,13 +2,13 @@
 #!/bin/sh
 echo "Saving output log to /var/log/great-problems/output.log"
 ADDRESS=127.0.0.1
-(
-        cd dist &&
-                ./backend \
-                         --hostname=greatproblems.ca \
-                         --address=$ADDRESS \
-                         --port=8000 \
-                         --access-log=/var/log/great-problems/access.log \
-                         --error-log=/var/log/great-problems/error.log \
-                &>> /var/log/great-problems/output.log
-)
+nix-shell -A shells.ghc -v --run '(
+cd dist &&
+./backend \
+		  --hostname=greatproblems.ca \
+		  --address=$ADDRESS \
+		  --port=8000 \
+		  --access-log=/var/log/great-problems/access.log \
+		  --error-log=/var/log/great-problems/error.log \
+		  &>> /var/log/great-problems/output.log
+)'
