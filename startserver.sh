a278cf0c7a3c0fae6687816de5006bcac1269dde
SlimTim10 <slimtim10@gmail.com>
2022-09-22 05:57:52 -0400

    Fix script

--- startserver.sh
+++ startserver.sh
@@ -1,14 +1,14 @@
 
 #!/bin/sh
 echo "Saving output log to /var/log/great-problems/output.log"
-nix-shell -A shells.ghc -v --run '(
+nix-shell -A shells.ghc -v --run '
 ADDRESS=127.0.0.1;
-cd dist &&
+cd dist;
 ./backend \
 		  --hostname=greatproblems.ca \
 		  --address=$ADDRESS \
 		  --port=8000 \
 		  --access-log=/var/log/great-problems/access.log \
 		  --error-log=/var/log/great-problems/error.log \
-		  &>> /var/log/great-problems/output.log
-)'
+		  &>> /var/log/great-problems/output.log;
+return'
