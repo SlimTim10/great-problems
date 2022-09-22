dfb6b8537d2e046abbbae4f25b6575e54b521933
SlimTim10 <slimtim10@gmail.com>
2022-09-22 10:14:35 -0400

    Update hash for problem2tex

--- dep-ext/problem2tex/default.nix
+++ dep-ext/problem2tex/default.nix
@@ -13,5 +13,5 @@ buildGoModule rec {
   };
 
   # modSha256 = pkgs.stdenv.lib.fakeSha256;
-  modSha256 = "sha256-EQ4BBpIhAY78HIHrPkxm8as+LF0uEPancQkvNq132Gk=";
+  modSha256 = "sha256-FoAdAxJHyCL/EOEwwZjZWAuqpwmWHPJu6kyLYlFQ8kg=";
 }
