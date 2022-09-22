3546337241bb9bfa1edfc6d4cde1d553382a76ad
SlimTim10 <slimtim10@gmail.com>
2022-09-21 20:58:14 -0400

    Update hash for dependency

--- dep-ext/ltspice2svg/default.nix
+++ dep-ext/ltspice2svg/default.nix
@@ -7,9 +7,10 @@ buildGoModule rec {
     owner = "icewire314";
     repo = "ltspice2svg";
     rev = "v${version}";
+    # sha256 = pkgs.stdenv.lib.fakeSha256;
     sha256 = "sha256-70728voXCS0Qssr4qL7afgx2BPWfsL17ggiY0jQa0F0=";
   };
 
   # modSha256 = pkgs.stdenv.lib.fakeSha256;
-  modSha256 = "sha256-C9DW1szZjwlth0R28oEKrnlw92wj2vgvz2Tgrt9wKgo=";
+  modSha256 = "sha256-F+z3Ji7AwzKF2DmbAY6L8ghvYKxlIxmcJchMfS7rLRI=";
 }
