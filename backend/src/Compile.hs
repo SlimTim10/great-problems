6f4ad977af5bb44de5ccdcabe5187d76a661462f
SlimTim10 <slimtim10@gmail.com>
2022-10-19 12:14:09 -0400

    Bug fix: compile in rapid succession
    The bug was due to using System.Directory.withCurrentDirectory in a multithreaded environment. The current working directory is a global state shared among all threads of the process. Now, all file paths are absolute and System.Directory is no longer imported.

--- backend/src/Compile.hs
+++ backend/src/Compile.hs
@@ -6,7 +6,7 @@ module Compile
 import qualified System.Process
 import qualified System.IO.Temp
 import qualified Data.Text.IO
-import qualified System.Directory
+import System.FilePath ((</>))
 
 import Common.Lib.Prelude
 import qualified Common.Api.Figure as Figure
@@ -23,28 +23,30 @@ compile
 compile contents randomSeed figures = do
   -- Do all of the work within a temporary directory (deleted after)
   -- e.g., /tmp/prb-e4bd89e5d00acdee
-  System.IO.Temp.withSystemTempDirectory "prb" $ \prbDir ->
-    System.Directory.withCurrentDirectory prbDir $ do
+  -- Note: It may be tempting to use System.Directory.withCurrentDirectory, but it is problematic in this multithreaded environment because the current working directory is a global state shared among all threads of the process.
+  -- (see: https://hackage.haskell.org/package/directory-1.3.8.0/docs/System-Directory.html#v:setCurrentDirectory)
+  -- It is important to only use absolute paths.
+  System.IO.Temp.withSystemTempDirectory "prb" $ \prbDir -> do
     -- Create files for problem2tex
     -- Problem: main.prb
     -- Figures: schem1.asc, schem2.asc, ..., fig1.svg, fig2.svg, ...
-    createFiles
+    createFiles prbDir
 
     -- Run problem2tex on files (creates new files)
     -- New problem: main.org
     -- New figures: schem1NEWasc.svg, schem2NEWasc.svg, ..., fig1NEW.svg, fig2NEW.svg, ...
-    runProblem2tex
+    runProblem2tex prbDir
 
     -- Run emacs on main.org to export HTML
     -- Creates main.html (with embedded SVGs)
-    runEmacs
+    runEmacs prbDir
 
     -- Read and return main.html contents as text
-    Right <$> Data.Text.IO.readFile "main.html"
+    Right <$> Data.Text.IO.readFile (prbDir </> "main.html")
   where
-    createFiles = do
+    createFiles dir = do
       -- Problem: main.prb
-      Data.Text.IO.writeFile "main.prb" contents
+      Data.Text.IO.writeFile (dir </> "main.prb") contents
       -- Figures: schem1.asc, schem2.asc, ..., fig1.svg, fig2.svg, ...
       void $ mapM_
         (\figure -> do
@@ -54,19 +56,19 @@ compile contents randomSeed figures = do
         )
         figures
         
-    runProblem2tex = do
+    runProblem2tex dir = do
       System.Process.callProcess
         "problem2tex"
-        [ "-export=main.org"
+        [ "-export=" <> dir </> "main.org"
         , "-random=" <> show randomSeed
-        , "main.prb"
+        , dir </> "main.prb"
         ]
 
-    runEmacs = do
+    runEmacs dir = do
       -- emacs main.org --batch -f org-html-export-to-html --kill
       System.Process.callProcess
         "emacs"
-        [ "main.org"
+        [ dir </> "main.org"
         , "--batch"
         , "-f", "org-html-export-to-html"
         , "--kill"
